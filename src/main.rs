01a4932b5be4060ea0ad043ae9ed7eb2f328a6af
Developer X <80836090+developerx-official@users.noreply.github.com>
2023-02-14 00:23:23 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,7 @@ fn main() -> Result<(), anyhow::Error> {
         "My egui App",
         options,
         Box::new(|_cc| Box::<app_logic::MyApp>::default()),
-    ).unwrap();
+    )
+    .unwrap();
     Ok(())
 }
