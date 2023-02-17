ad451491ad8b0de0cd619ff0141b033d3328c0f2
Developer X <jmitchellvideoproject@gmail.com>
2023-02-17 02:16:18 -0500

    Initial commit to branch

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 // hide console on Windows in release
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+// #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
 use eframe::{egui, HardwareAcceleration};
+use self_update::cargo_crate_version;
 
 mod app_logic;
 
 fn main() -> Result<(), anyhow::Error> {
+    let _status = self_update::backends::github::Update::configure()
+        .repo_owner("developerx-official")
+        .repo_name("ui_test")
+        .bin_name("ui_test")
+        .show_download_progress(true)
+        .current_version(cargo_crate_version!())
+        .build()?
+        .update()?;
+    #[cfg(target_os = "windows")]
+    winconsole::window::hide();
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(640_f32, 480_f32)),
         resizable: true,
